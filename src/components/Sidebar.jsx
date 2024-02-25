02584551e34d0587bb5143b92e4460023d8604ba
katyjayne <katyjayneblank@gmail.com>
2024-02-25 14:39:19 -0500

    cleaned up deleted and commented out code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,15 @@
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
 import Login from './Login';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import Dashboard from './Dashboard';
+
 
 
 export default function TemporaryDrawer() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
@@ -27,13 +20,10 @@ export default function TemporaryDrawer() {
       <List>
         {['Login'].map((text) => (
           <ListItem key={Login} disablePadding>
-              <Login>
-                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-              </Login>
+              <Login />
           </ListItem>
         ))}
       </List>
-      {/* <Divider /> */}
     </Box>
   );
 
@@ -47,11 +37,3 @@ export default function TemporaryDrawer() {
   );
 }
 
-
-// export default function Sidebar() {
-//   return (
-//     <>
-//     <Login />
-//     </>
-//   )
-// }
